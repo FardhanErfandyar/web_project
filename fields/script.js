3b158900a4c7d799f9d12715ae9cd7004d77cee6
Fardhan Erfandyar <fardh.er@gmail.com>
2024-02-29 09:22:49 +0700

    revisi

---
+++ fields/script.js
@@ -0,0 +1,9 @@
+// var navbar = document.getElementById("navbar");
+
+//         window.onscroll = function () {
+//             if (window.pageXOffset >= navbar.offsetTop) {
+//                 navbar.classList.add("sticky");
+//             } else {
+//                 navbar.classList.remove("sticky");
+//             }
+//         };
