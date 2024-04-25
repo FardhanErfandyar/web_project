69d0d66a6342c83501a34b5b9fb0c701f973a762
Fardhan Erfandyar <fardh.er@gmail.com>
2024-04-26 01:01:31 +0700

    slider image

--- fields/resources/views/detail.blade.php
+++ fields/resources/views/detail.blade.php
@@ -6,9 +6,24 @@
 @section('detail')
     <div id="detail">
         <h2>{{ $post->name}}</h2>
-        @foreach($post->images as $image)
-            <img src="{{ asset('storage/'.$image->image) }}" alt="{{ $post->name }}">
-        @endforeach
+
+        <section class="container-image">
+            <div class="slider-wrapper">
+                <div class="slider">
+                    @foreach($post->images as $image)
+                        <img id="slide-{{ $loop->index }}" src="{{ asset('storage/'.$image->image) }}" alt="{{ $post->name }}">
+                     @endforeach
+                </div>
+                <div class="slider-nav">
+                    @foreach($post->images as $image)
+                        <a href="#slide-{{ $loop->index }}"></a>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+
+
+      
         <h3>Alamat: {{ $post ->address }}</h3>
         <h3>Harga: {{ $post->price }}</h3>
         <h3>Kecamatan: {{ $post->district->name }}</h3>
@@ -16,3 +31,4 @@
         <h3>Fasilitas: {{ $post->facility }}</h3>
     </div>
 @endsection
+
